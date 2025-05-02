98e3da70242e6748fc8588670bd858e7250f52ad
mu <89599883+muhimmatularofah@users.noreply.github.com>
2025-05-02 19:48:02 +0700

    Update web_mba.py

--- web_mba.py
+++ web_mba.py
@@ -48,7 +48,7 @@ cleaned['nama_barang'] = cleaned['nama_barang'].astype(str)
 df_sorted = cleaned.groupby('TRX_ID', group_keys=False).apply(lambda x: x.sort_values('nama_barang'))
 trx_count = len(df_sorted)
 item_count = len(pd.unique(df_sorted['nama_barang']))
-txid_count = dfx['TRX_ID'].nunique()
+txid_count = output['TRX_ID'].nunique()
 
 
 df_sorted['year_month'] = df_sorted['date'].dt.to_period('M')
