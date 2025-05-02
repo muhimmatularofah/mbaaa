e96062228b61d5b7acd141c7a6053a3e37d2832e
mu <89599883+muhimmatularofah@users.noreply.github.com>
2025-05-02 18:53:46 +0700

    Update web_mba.py

--- web_mba.py
+++ web_mba.py
@@ -4,7 +4,7 @@ import pandas as pd
 uploaded_files = st.file_uploader(
     "Choose a CSV file", accept_multiple_files=True
 )
-all data = pd.DataFrame()
+all_data = pd.DataFrame()
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
