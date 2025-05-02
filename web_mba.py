63a6c4a7b0ad04a2858d649faceb3c3c1c2918a0
mu <89599883+muhimmatularofah@users.noreply.github.com>
2025-05-02 18:35:34 +0700

    Update web_mba.py

--- web_mba.py
+++ web_mba.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+    
 # Unduh file CSV dari Google Drive
 output = 'data.csv'  # nama file setelah diunduh
 dfx = pd.read_csv("sorted_by_item.csv")
