c907092511b077b8ac422a95d150cfbce62221d7
mu <89599883+muhimmatularofah@users.noreply.github.com>
2025-05-02 19:01:04 +0700

    Update web_mba.py

--- web_mba.py
+++ web_mba.py
@@ -1,17 +1,33 @@
 import streamlit as st
 import pandas as pd
 
+#uploaded_files = st.file_uploader(
+#    "Choose a CSV file", accept_multiple_files=True
+#)
+#all_data = pd.DataFrame()
+#for uploaded_file in uploaded_files:
+#    bytes_data = uploaded_file.read()
+#    st.write("filename:", uploaded_file.name)
+#    st.write(bytes_data)
+    
+#    output = pd.read_csv (uploaded_file)
+#    all_data = pd.contact([all_data, output], ignore_index=True)
+
 uploaded_files = st.file_uploader(
     "Choose a CSV file", accept_multiple_files=True
 )
 all_data = pd.DataFrame()
+
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    # Menghapus atau mengomentari bagian ini untuk tidak mencetak nama file dan data
+    # st.write("filename:", uploaded_file.name)
+    # st.write(bytes_data)
     
-    output = pd.read_csv (uploaded_file)
-    all_data = pd.contact([all_data, df], ignore_index=True)
+    # Membaca file CSV dan menggabungkan data
+    output = pd.read_csv(uploaded_file)
+    all_data = pd.concat([all_data, output], ignore_index=True)
+
 
 # Unduh file CSV dari Google Drive
 # output = 'data.csv'  # nama file setelah diunduh
